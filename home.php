921c7a275aea7b24633446f4877fd5794d6853be
Joao Luiz <contato.joaolfc@hotmail.com>
2019-03-13 19:58:00 -0300

    copiando site storm

---
+++ home.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Storm Group</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="java.js"></script>
+	<meta charset="utf-8">
+	<meta id="viewport" name="viewport" content="width=device-width, user-scalable=0">
+</head>
+<body>
+	<div class="topo">
+		<div class="topo_int">
+			<div class="logo">
+				<img src="novalogo.png">
+			</div>
+			<div class="menu">
+				<div class="linha_1"></div>
+				<div class="linha_1"></div>
+				<div class="linha_1"></div>
+
+				
+			</div>
+
+
+		</div>
+	</div>	
+
+	<div class="banner">
+		<div class="banner_int">
+			<div class="prod">PRODUTOS</div>
+			<div class="plat"><b>Plataforma de Video</b></div>
+			<div class="desc">A solução de streaming STORM garante a entrega de conteudo de áudio e vídeo nas mais diversas plataformas e resoluções em qualquer qualidade banda de internet.</div>
+			<div class="botao"><a href="#"> LEIA MAIS</a></div>
+			
+
+
+
+
+		</div>
+	</div>	
+
+
+	<div class="container">
+		<div class="tittle_box">Conheça a Storm</div>
+		<div class="linha"></div>
+
+		<div class="text">
+			<b>Na Storm a inovação é o que perseguimos!</b><br><br><br>
+			As aplicações Storm vão desde simples necessidades à integrações entre sistemas, manipulação de hardwares e regras de negócios complexas.<br><br><br>
+			Desenvolvemos soluções inteligentes e integradas para web, sistemas, tecnologia embarcada em drones, API's, segurança da informação, infraestrutura de alta performance, aplicativos mobile Android e iOS e aplicativos para Smart TVs Sony, LG, Samsung, Phillips e Panasonic.<br><br><br>
+			Dispomos de uma equipe multidisciplinar capaz de transformar ideias inovadoras em realidade. Utilizando metodologias ágeis aliados a qualidade e prazo, otimizamos o tempo de concepção e publicação dos produtos, gerando assim, mais valor para o cliente.<br><br><br>
+			Temos conhecimento técnico e inteligência para atender ao seu desafio. Seja qual for a necessidade tecnológica de sua empresa, temos uma solução inovadora para você.<br><br><br>
+		</div>
+		
+		<div class="botao_2">
+			<div class="button-storm">SAIBA MAIS</div>
+		</div>	
+
+	</div>	
+
+	<div class="clientes">
+		<div class="tittle_box">Nossos Clientes</div>
+		<div class="linha_54"></div>
+		<article>
+		<ul>
+			<li> <img src="images/accenture.png"> </li>
+			<li> <img src="images/bankofamerica.png"></li>
+			<li> <img src="images/esport.png"></li>
+			<li><img src="images/exercitobrasileiro.png"></li>
+			<li><img src="images/globo.png"></li>
+			<li><img src="images/globocom.png"></li>
+			<li><img src="images/horizonte.png"></li>
+			<li><img src="images/megapix.png"></li>
+			<li><img src="images/multishow.png"></li>
+			<li><img src="images/philos.png"></li>
+			<li><img src="images/sexyhot.png"></li>
+			<li><img src="images/sporttv.png"></li>
+			<li><img src="images/telecine.png"></li>
+			<li><img src="images/turner.png"></li>
+			<li><img src="images/universalchanel.png"></li>
+			<li><img src="images/viva.png"></li>
+			<li><img src="images/logo-globoplay-32.png"></li>
+			<li><img src="images/logo-globosat-play-11.png"></li>
+			<li><img src="images/logo-newcine-34.png"></li>
+		</ul>	
+		</article>
+
+		<div class="linha_3"></div>
+
+		<div class="depoimento">
+			<div class="depo_box">
+				<div class="depo_tittle">Depoimentos</div>
+				<div class="depo_int">A Storm vem constantemente ajudando a Globosat a desenvolver sites e aplicativos de sucesso. A eficiência e eficácia dessa parceria é consequência de 3 fatores fundamentais na implementação de produtos para as novas mídias digitais: uma qualidade impecável na entrega de sua camada de apresentação, um código robusto e escalável no background e muita flexibilidade para tratar requisitos de negócio cada vez mais dinâmicos.</div>
+
+				<div class="assinatura"><b>André Nava - Gerente de novas mídias - Globosat</b></div>
+
+
+
+
+			
+
+			
+			</div>	
+
+		</div>
+	</div>
+
+	<div class="linha_4"></div>	
+
+	<div class="rodape">
+		<div class="rodape_int">
+			<div class="box_1">
+				<div class="img_logo"><img src="novalogo.png"></div>
+
+				<div class="cont_1">
+
+					Rio de Janeiro | Rua José de Figueiredo 320, Unid. 29 e 30 <br>
+					Barra da Tijuca, Rio de Janeiro/RJ - CEP: 22793-170 <br>
+					Office: +55 (21) 3734-0695 
+					
+				</div>
+
+
+				<div class="cont_2">
+					New York | 136 Madison Avenue, 5th Floor <br>
+					New York, NY 10016 <br>
+					Office: +1 (646) 722 3838
+
+				</div>
+
+
+				<div class="cont_3">© 2018 STORMGROUP. TODOS OS DIREITOS RESERVADOS.
+
+
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+
+			<div class="box_2">
+				<nav>
+					<ul>
+						
+						<li> A STORM</li>
+						<li>SERVIÇOS</li>
+						<li> PRODUTOS</li>
+						<li> DRONES</li>
+						<li> CASES</li>
+						<li> BLOG</li>
+						<li> TRABALHE CONOSCO</li>
+						<li> CONTATO</li>
+					</ul>
+				</nav>
+			</div>
+
+
+
+		</div>	
+	</div>	
+
+</body>
+</html>
